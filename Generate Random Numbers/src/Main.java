f96c6443809bb79d1c5d24cd20310b176f0d2ae6
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-06-19 09:16:21 +0700

    Java Random Method

---
+++ Generate Random Numbers/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int number1;
+        int number2;
+        int number3;
+        double num;
+        boolean isHeads;
+
+        number1 = random.nextInt(1, 101);
+        number2 = random.nextInt(1, 101);
+        number3 = random.nextInt(1, 101);
+        num = random.nextDouble();
+        isHeads = random.nextBoolean();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(num);
+
+        if(isHeads) {
+            System.out.println("Heads");
+        }
+        else {
+            System.out.println("Tails");
+        }
+
+    }
+}
