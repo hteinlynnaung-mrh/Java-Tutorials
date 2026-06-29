a0cd381c9485d1d6bcac0335ff78894b84b0065c
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-06-29 13:31:17 +0700

    Java for loops

---
+++ For loops/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // for loops = execute some code a CERTAIN amount of times
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        for(int i = 0; i <= 10; i+= 2) {
+            System.out.println(i);
+        }
+
+        for(int i = 10; i >= 0; i-= 2) {
+            System.out.println(i);
+        }
+
+        
+
+    }
+}
---
+++ For loops/src/Main1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for(int i = 1; i <= max; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
