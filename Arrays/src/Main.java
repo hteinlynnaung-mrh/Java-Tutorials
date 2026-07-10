28d9646531d6ac7683dab460927493ff298a2bf1
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-07-10 10:01:10 +0700

    Java Arrays

---
+++ Arrays/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // array = a collection of values of the dame data type
+        //         * think of it as a variable that can store more than 1 value
+
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+        // fruits[1] = "pineapple";
+        int numOfFruits = fruits.length;
+        //  Arrays.sort(fruits);
+        //  Arrays.fill(fruits, "pineapple");
+
+        for(int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[0]);
+        //  System.out.println(fruits[i]);
+        }
+        //  System.out.println(fruits[i] + " "); =
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        System.out.println(fruits[1]);
+        System.out.println(numOfFruits);
+    }
+}
