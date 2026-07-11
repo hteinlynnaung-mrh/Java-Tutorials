d3e0313ed17b19ea72bc16a789c36bd40e21d095
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-07-11 10:02:24 +0700

    Java Arrays

---
+++ Arrays/src/UserInputIntoArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInputIntoArray {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] foods = new String[3];
+
+        for(int i = 0; i < foods.length; i++) {
+            System.out.print("Enter a food: " );
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food: foods) {
+            System.out.println(food);
+        }
+
+        scanner.close();
+
+    }
+}
