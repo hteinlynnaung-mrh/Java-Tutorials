6d6b5889dd2d4be7967c3bf28839ad66fa2b134f
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-07-12 09:52:29 +0700

    Java Array

---
+++ Arrays/src/SearchArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SearchArray {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // int[] numbers = {1, 9, 2, 8, 3, 5, 4};
+        String[] fruits = {"apple", "orange", "banana"};
+        // int target = 8;
+        boolean isFound = false;
+        String target;
+
+        System.out.print("Enter a fruit to search for: ");
+        target = scanner.nextLine();
+
+        for(int i = 0; i < fruits.length; i++) {
+            if(fruits[i].equals(target)){
+        //  if(target == numbers[i]){
+                System.out.println("Element found at index: " + i);
+                isFound = true;
+                break;
+            }
+        }
+
+        if(!isFound) {
+            System.out.println("Element not found in the array");
+        }
+
+        scanner.close();
+    }
+}
