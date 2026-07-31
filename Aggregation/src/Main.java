81ee7fdffe88175fc2054c649e33331f2fb02254
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-07-31 08:54:10 +0700

    Java Aggregation

---
+++ Aggregation/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Aggregation = Represents a "has-a" relationship between objects.
+        //               One object contains another object as part of its structure,
+        //               but the contained object/s can exist independently.
+
+        Book book1 = new Book("The Fellow of the Ring", 423);
+        Book book2 = new Book("The Pleasure of the Nature", 370);
+        Book book3 = new Book("The Sealing in the Sea", 400);
+
+        Book[] books = {book1, book2, book3};
+
+        Library library = new Library("NYC Public Library", 1897, books);
+
+        library.displayInfo();
+    }
+
+}
