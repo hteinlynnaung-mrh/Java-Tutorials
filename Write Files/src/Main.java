1e6214b1be062fb5a615e2a039eb52772b631616
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-08-05 13:34:16 +0700

    Java WRITE FILES

---
+++ Write Files/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // How to write al file using Java (4 popular options)
+
+        String filePath = "/Users/hteinlynnaung/Downloads/text.txt";
+        String textContent = """
+                Roses are Red
+                Violets are Blue
+                BOOTY BOOTY BOOTY
+                ROCKIN' EVERYWHERE!
+                """;
+
+        try(FileWriter writer = new FileWriter("/Users/hteinlynnaung/Downloads/test.txt")) {
+            writer.write(textContent);
+            System.out.println("File has been written");
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Could not locate file location");
+        }
+        catch(IOException e) {
+            System.out.println("Could not write file");
+        }
+
+        // FileWriter = Good for small or medium-sized text files
+        // BufferedWriter =Better performance for large amounts of text
+        // PrintWriter = Best for structured data, like reports or logs
+        // FileOutputStream = Best for binary files (e.g., images, audio files)
+
+
+    }
+}
