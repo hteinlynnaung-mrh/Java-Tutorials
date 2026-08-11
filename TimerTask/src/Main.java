9f2f75788aea44c98a4d610ea0e889786cd89f20
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-08-11 22:45:42 +0700

    Java TimerTasks

---
+++ TimerTask/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Timer = Class that schedules tasks at specific times or periodically
+        //         Useful for: sending notifications, scheduled updates, repetitive actions
+
+        // TimerTask = Represents the task that will be executed by the Timer
+        //             You will extend the TimerTask class to define your task
+        //             Create a subclass of TimerTask and @Override run()
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = 3;
+
+            @Override
+            public void run(){
+                System.out.println("Hello!");
+                count--;
+                if(count < 0){ // count from 3 to less than 0 -> total = 4 times
+                    System.out.println("TASK COMPLETE!");
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.schedule(task,1000, 2000);
+        // delay = first time execution
+        // period = repeating time from the second time
+
+    }
+}
