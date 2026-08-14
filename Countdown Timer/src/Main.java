98a8f1397cb5e06068a5ce70b12a1f882354a47f
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-08-14 20:38:07 +0700

    Java Countdown Timer

---
+++ Countdown Timer/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Java COUNTDOWN TIMER PROGRAM
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter # of seconds to countdown from: ");
+        int response = scanner.nextInt();
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = response;
+
+            @Override
+            public void run(){
+
+                System.out.println(count);
+                count--;
+                if(count < 0){
+                    System.out.println("HAPPY NEW YEAR!");
+                    System.out.println("I Love You 西上ちゃん");
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 1000); // (task, delay, period)
+
+    }
+
+}
