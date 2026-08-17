16a8c6da63d4a04a0ea83c649178b8a53c03a8a7
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-08-17 11:07:16 +0700

    Java Enums

---
+++ Enums/src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Enums = (Enumerations) A special kind of class that
+        //         represents a fixed set of constants.
+        //         They improve code readability and are easy to maintain.
+        //         More efficient with switches when comparing Strings.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
+        try {
+            Day day = Day.valueOf(response);
+
+            switch (day) {
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FIRDAY -> System.out.println("It is a weekday!");
+                case SATURDAY,
+                     SUNDAY -> System.out.println("It is the weekend!");
+            }
+        }
+        catch(IllegalArgumentException e) {
+            System.out.println("Please enter a valid day!");
+        }
+
+        // System.out.println(day);
+        // System.out.println(day.getDayNumber());
+
+        scanner.close();
+    }
+}
